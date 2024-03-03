632c8d62596db051a5c7f940d5f9f06fca4d84ca
Rahul Soni <rahuls5788@gmail.com>
2024-03-04 01:53:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    
+  
+
+    
+    </>
+  )
+}
+
+export default App
